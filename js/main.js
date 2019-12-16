6c518d2bb5b1f05381d4650409af34060b03454f
felixderf <felikslisovskyi@gmail.com>
2019-12-16 22:20:59 +0200

    third commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.clients__item').css("height", $('.clients__item').css("width"));
+$('.clients__item').css('height', $('.clients__item').css('width'));
 
 $(document).ready(function () { 
     var $win_n = $(window).width();
@@ -19,15 +19,31 @@ $(document).ready(function () {
     if($win_n<=1223){
         $('.main-nav__main-item span').click(function () {
             if($(this).parent().children('.main-nav__sub-menu').css("display")=="none"){
-                $(this).css({"border-left":"10px solid transparent", "border-top":"10px solid #fff"});
+                $(this).css({"border-left":"10px solid transparent", "border-top":"10px solid #fff", "margin-right":"5px"});
                 $('.main-nav__sub-menu').hide('normal');
                 $(this).parent().children('.main-nav__sub-menu').show('normal');
                 $('.main-nav__main-item').removeClass('active');
                 $(this).parent().addClass('active');
             } else {
-                $(this).css({"border-left":"10px solid #fff", "border-top":"10px solid transparent"});
+                $(this).css({"border-left":"10px solid #fff", "border-top":"10px solid transparent", "margin-right":"0"});
                 $('.main-nav__sub-menu').hide('normal');
                 $('.main-nav__main-item').removeClass('active');
+            }
+        })
+        $('.main-nav__link').click(function (e) {
+            if($(this).parent().children('span').length > 0) {
+                e.preventDefault();
+                if($(this).parent().children('.main-nav__sub-menu').css("display")=="none"){
+                    $(this).parent().children('span').css({"border-left":"10px solid transparent", "border-top":"10px solid #fff", "margin-right":"5px"});
+                    $('.main-nav__sub-menu').hide('normal');
+                    $(this).parent().children('.main-nav__sub-menu').show('normal');
+                    $('.main-nav__main-item').removeClass('active');
+                    $(this).parent().addClass('active');
+                } else {
+                    $(this).parent().children('span').css({"border-left":"10px solid #fff", "border-top":"10px solid transparent", "margin-right":"0"});
+                    $('.main-nav__sub-menu').hide('normal');
+                    $('.main-nav__main-item').removeClass('active');
+                }
             }
         })
     } else {
